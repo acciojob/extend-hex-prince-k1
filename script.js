d82e4e4fae7a3e9e0f2e72d5fda1a49a86ac98cd
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-10 19:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let arr = shortHex.split('');
+	let result = ['#'];
+	for(let i = 1; i < arr.length; i++){
+		result.push(arr[i]);
+		result.push(arr[i]);
+	}
+	return result.join('');
 };
 
 // Do not change the code below.
