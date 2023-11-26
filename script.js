a30080e8bd4d5bbb31e53b140515466cf14f4937
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-26 21:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ const extendHex = (shortHex) => {
 		for(let i = 1; i < arr.length; i++){
 			result.push(arr[i].toUpperCase());
 			result.push(arr[i].toUpperCase());
+			
 		}
 		return result.join('');
 	}
-	for(let i = 1; i < arr.length; i++){
+	for(let i = 0; i < arr.length; i++){
+		if(i == 0){
+			if(arr[0] != "#"){
+				result.push(arr[0]);
+				result.push(arr[0]);
+			}
+		}else{
 		result.push(arr[i]);
 		result.push(arr[i]);
+		}
 	}
 	return result.join('');
 };
